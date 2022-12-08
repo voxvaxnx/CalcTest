3fe54f945e442e083d569e2c8077e2b84ac51551
voxvaxnx <voxvaxnx@yandex.ru>
2022-12-08 22:50:38 +0500

    commitCalcTest2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
-    public Main() {
-    }
-
-    public static void main(String[] args) {
+       public static void main(String[] args) {
         System.out.println("Введите выражение:");
         Scanner scan = new Scanner(System.in);
         String line = scan.nextLine();
@@ -20,10 +17,8 @@ public class Main {
             System.exit(0);
         }
 
-        int i;
-        int j;
-        for(i = 0; i <= 9; ++i) {
-            for(j = 0; j <= 9; ++j) {
+        for(int i = 0; i <= 9; ++i) {
+            for(int j = 0; j <= 9; ++j) {
                 if (words[0].equals(iskInt[i]) && words[2].equals(iskInt[j])) {
                     Calcul arab = new Calcul(Integer.valueOf(words[0]), words[1], Integer.valueOf(words[2]));
                     String lastText = "Ответ: " + arab.lastText;
@@ -32,16 +27,16 @@ public class Main {
             }
         }
 
-        for(i = 0; i <= 9; ++i) {
-            for(j = 0; j <= 9; ++j) {
+        for(int i = 0; i <= 9; ++i) {
+            for(int j = 0; j <= 9; ++j) {
                 if (words[0].equals(iskString[i]) && words[2].equals(iskString[j])) {
                     Rom2Arab out = new Rom2Arab();
                     int vol1 = out.toArab(words[0]);
                     int vol2 = out.toArab(words[2]);
                     Calcul conv = new Calcul(vol1, words[1], vol2);
                     if (conv.lastText >= 1) {
-                        String var10000 = out.toRom(conv.lastText);
-                        String lastText = "Ответ: " + var10000;
+                        String romConv = out.toRom(conv.lastText);
+                        String lastText = "Ответ: " + romConv;
                         return lastText;
                     }
 
